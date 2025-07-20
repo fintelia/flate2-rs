2ca8586897249dc27398e000f16342523075fd76
Jonathan Behrens <fintelia@gmail.com>
2025-07-19 18:00:07 -0700

    Correct description of FlushCompress::Partial

--- src/mem.rs
+++ src/mem.rs
@@ -55,9 +55,7 @@ pub enum FlushCompress {
     ///
     /// All input data so far will be available to the decompressor (as with
     /// `Flush::Sync`). This completes the current deflate block and follows it
-    /// with an empty fixed codes block that is 10 bits long, and it assures
-    /// that enough bytes are output in order for the decompressor to finish the
-    /// block before the empty fixed code block.
+    /// with an empty fixed codes block that is 10 bits long.
     Partial = ffi::MZ_PARTIAL_FLUSH as isize,
 
     /// All pending output is flushed to the output buffer and the output is
